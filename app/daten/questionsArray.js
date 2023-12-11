1b75084d38f76b7723c701dd86d9966faf538a02
Blooz <89782115+mumbablooz@users.noreply.github.com>
2023-12-11 10:01:55 +0100

    Add files via upload

--- app/daten/questionsArray.js
+++ app/daten/questionsArray.js
@@ -1,7 +1,52 @@
 export const questionsArray =[
-    'Wie funktioniert Wissenschaft?',
-    'Gibt es den Klimawandel?',
-    'Was sind die Grenzen des Wachstums?',
-    //'Was ist das Judentum?',
-    'Was ist Atomkraftenergie?',
+{
+        question: "Wie funktioniert Wissenschaft?",
+        answered: "inProgress",
+        answers: []
+},
+{
+    question: "Gibt es den Klimawandel?",
+    answered: "inProgress",
+    answers: []
+},
+{
+    question: "Was sind die Grenzen des Wachstums?",
+    answered: "inProgress",
+    answers: []
+},
+{
+    question: "Was ist Atomkraftenergie?",
+    answered: "inProgress",
+    answers: []
+},
+{
+    question: "Ist Rudolf Steiner ein Rassist?",
+    answered: "inProgress",
+    answers: []
+},
+{
+question: "Fließt der Rassismus von Rudolf Steiner auch in seine Überlegungen?",
+answered: "inProgress",
+answers: []
+},
+{
+question: "Werden diese Überlegungen heute von Walddorfpädagogen übernommen?",
+answered: "inProgress",
+answers: []
+},
+{
+question: "Wird Rudolf Steiner von den Walddorf Pädagogen verehrt oder dizanzieren sich die Waldorfeinrichtungen heute von ihm?",
+answered: "inProgress",
+answers: []
+},
+{
+    question: "Ist die Waldorfpädagogig eine wissenschaftlich belegte Pädagogik? Gibt es Studien die das beweisen?",
+    answered: "inProgress",
+    answers: []
+    },
+    {
+    question: "Ist Anthropologie eine echte Wissenschaft? Gibt es Studien die das beweisen?",
+    answered: "inProgress",
+    answers: []
+    },
 ]
