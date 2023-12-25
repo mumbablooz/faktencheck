111b7bdd90af951d2670a3473ab97b0610d2ab1c
Blooz <89782115+mumbablooz@users.noreply.github.com>
2023-12-25 08:59:25 +0100

    Add files via upload

--- app/questions/categorys/page.jsx
+++ app/questions/categorys/page.jsx
@@ -7,7 +7,7 @@ export default function Categorys() {
     <section>
       <h3>Categorien</h3>
 {categorysArray.map((category,categoryIndex)=>{
-    return (<div key={'category-'+index}
+    return (<div key={'category-'+categoryIndex}
     style={{marginBottom: '0.6rem'}}>
       <Link href={`/questions/categorys/${categoryIndex}`}>
       <p>{category.name}</p></Link>
